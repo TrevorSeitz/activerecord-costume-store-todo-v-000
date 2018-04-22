dbfea4f3130bde96c29cd82415e2b384ee8c1556
Trevor Seitz <trevinla@gmail.com>
2018-04-22 16:03:56 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,7 +4,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
   def change
-    create_table :Costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.text :image_url
       t.float :price
